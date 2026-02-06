a9d254828d7a352f7042446f87c4f1ee6268958e
Flinn Fuchs <47865376+DiscoveryFox@users.noreply.github.com>
2026-02-06 07:28:38 +0000

    added user table

--- tools/orm.py
+++ tools/orm.py
@@ -1,3 +1,6 @@
+import os
+import sqlite3
+
 
 class Database:
     """
@@ -5,5 +8,21 @@ class Database:
         - First Iteration using a sqlite connection to the database
         - Second Iteration using Connection Pooling if we still got time. 
     """
-    def __init__(self):
-        pass
+    def __init__(self, path: str):
+        self.path = path
+        self.scheme = """
+CREATE TABLE User (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+        """
+
+    def initialize_database(self):
+        if not os.path.exists(self.path):
+            conn = sqlite3.connect(self.path)
+            cursor = conn.cursor()
+            cursor.execute(self.scheme)
+            conn.commit()
+            conn.close()
